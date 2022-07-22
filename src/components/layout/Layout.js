a9350f4a0b3e518d7924e985f7260f7387a9fbe9
bonevboyan <boyanbonev43@gmail.com>
2022-07-22 18:24:45 +0300

    Fixed Tab Sizing

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,15 +1,15 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
 
-import classes from './Layout.module.css';
-import MainNavigation from './MainNavigation';
+import classes from "./Layout.module.css";
+import MainNavigation from "./MainNavigation";
 
 const Layout = (props) => {
-  return (
-    <Fragment>
-      <MainNavigation />
-      <main className={classes.main}>{props.children}</main>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <MainNavigation />
+            <main className={classes.main}>{props.children}</main>
+        </Fragment>
+    );
 };
 
 export default Layout;
